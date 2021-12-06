d8888c0b13dc2b9e2ed2da94938b161fea2ebe80
Tavares <guleesin@gmail.com>
2021-12-06 00:54:09 -0300

    Api Web

---
+++ ApiFit/PccAPI/Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PccAPI.Models
+{
+    public class Login
+    {
+        public string Cpf { get; set; }
+        public string Senha { get; set; }
+
+        public Login(string r1, string r2)
+        {
+            this.Cpf = r1;
+            this.Senha = r2;
+        }
+    }
+}
