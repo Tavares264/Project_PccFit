c4d54a69fc9b3332aa29a2fd53f54e134eeff268
renato <renato78563@gmail.com>
2022-03-17 13:16:32 -0300

    alterações API

--- webPCC/JS/api.js
+++ webPCC/JS/api.js
@@ -1,3 +1,8 @@
+$(document).ready(function () 
+{
+    consultar('calcio')
+});
+
 function postar(a){
     var Nome = document.querySelector("#recipient-name");
 
@@ -17,15 +22,29 @@ function postar(a){
 function consultar(a){
     var url = `https://localhost:44333/api/Comentario?Assunto=${a}`;
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url, false);
-    xhttp.send();
+    $.get(url,data =>{
+        dados = JSON.parse(data)
+        var div = '';
 
-    valor = xhttp.responseText;
+        $(dados).each(function (index) {
+            
+            div += `<div class="d-flex">
+                <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                <div class="ms-3">
+                    <div class="fw-bold">${dados[index].nome}</div>
+                    ${dados[index].comentario}
+                </div>
+            </div><br>`
+        });
 
-    console.log(valor);
-    json = JSON.parse(JSON.parse(valor));
-    console.log(json);
+        document.getElementById("comentario").innerHTML =div;
+    })
     
+    //json = JSON.parse(JSON.parse(valor));
+    //console.log(json);
+ 
+    
+
     //"{'nome':'Tavares', 'email':'Tavas@gmail.com', 'comentario':'Teste Web', 'assunto':'Teste Web'}"
 }
+
