348d4aaa4452b9725ddb71ff7408b21696f6a639
Tavares <guleesin@gmail.com>
2022-04-18 20:38:48 -0300

    Api Agenda

---
+++ ApiFit/Controllers/AgendaController.cs
@@ -0,0 +1,150 @@
+﻿using PccAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Web.Script.Serialization;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace PccAPI.Controllers
+{
+    public class AgendaController : ApiController
+    {
+        private static List<Comentarios> Coments = new List<Comentarios>();
+        string myconn = ConfigurationManager.AppSettings["msconn"];
+        MySqlDataReader reader;
+
+        //Nutricionista
+        //Paciente
+        //Id
+        //Id_nutricionista
+        //Id_paciente
+        //Dt_agenda
+        //Horario
+        //Descricao
+
+        public string Post(string nome, string email, string comentario, string assunto)
+        {
+            if (!string.IsNullOrEmpty(comentario) && !string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(nome) && !string.IsNullOrEmpty(assunto))
+            {
+                try
+                {
+                    string query = "INSERT INTO tb_comentarios (nome, email, comentario, assunto) values (@Nome, @Email, @Coment, @Assunto)";
+                    using (MySqlConnection conexao = new MySqlConnection(myconn))
+                    {
+                        MySqlCommand command = new MySqlCommand(query, conexao);
+                        command.Parameters.AddWithValue("@Nome", nome);
+                        command.Parameters.AddWithValue("@Email", email);
+                        command.Parameters.AddWithValue("@Coment", comentario);
+                        command.Parameters.AddWithValue("@Assunto", assunto);
+
+                        conexao.Open();
+                        reader = command.ExecuteReader();
+                        conexao.Close();
+
+                        return @"{""Status"":""OK""}";
+                    }
+                }
+                catch
+                {
+                    return @"{""Status"":""ERRO""}";
+                }
+            }
+            else
+            {
+                return @"{""Status"":""Campos Vazios""}";
+            }
+        }
+
+
+
+        public string Get(string PCpf)
+        {
+            if (!string.IsNullOrEmpty(PCpf))
+            {
+                try
+                {
+                    string query = "SELECT * FROM vw_agenda WHERE pac_cpf = @PCpf;";
+                    using (MySqlConnection conexao = new MySqlConnection(myconn))
+                    {
+                        MySqlCommand command = new MySqlCommand(query, conexao);
+                        command.Parameters.AddWithValue("@PCpf", PCpf);
+
+                        conexao.Open();
+                        reader = command.ExecuteReader();
+
+                        List<dynamic> list = new List<dynamic>();
+
+                        while (reader.Read())
+                        {
+                            string Paciente = Convert.ToString(reader["paciente"]);
+                            string Dt_agenda = Convert.ToString(reader["dt_agenda"]);
+                            string Horario = Convert.ToString(reader["horario"]);
+
+                            list.Add(new { paciente = Paciente, dt_agenda = Dt_agenda, horario = Horario });
+                        }
+                        conexao.Close();
+                        return JsonConvert.SerializeObject(list);
+
+                    }
+                }
+                catch
+                {
+
+                    return @"{""Status"":""ERRO""}";
+                }
+            }
+            else
+            {
+                return @"{""Status"":""Campos Vazios""}";
+            }
+        }
+
+        public string GET(string NCpf)
+        {
+            if (!string.IsNullOrEmpty(NCpf))
+            {
+                try
+                {
+                    string query = "SELECT * FROM vw_agenda WHERE nut_cpf = @NCpf;";
+                    using (MySqlConnection conexao = new MySqlConnection(myconn))
+                    {
+                        MySqlCommand command = new MySqlCommand(query, conexao);
+                        command.Parameters.AddWithValue("@NCpf", NCpf);
+
+                        conexao.Open();
+                        reader = command.ExecuteReader();
+
+                        List<dynamic> list = new List<dynamic>();
+
+                        while (reader.Read())
+                        {
+                            string Paciente = Convert.ToString(reader["paciente"]);
+                            string Dt_agenda = Convert.ToString(reader["dt_agenda"]);
+                            string Horario = Convert.ToString(reader["horario"]);
+
+                            list.Add(new { paciente = Paciente, dt_agenda = Dt_agenda, horario = Horario });
+                        }
+                        conexao.Close();
+                        return JsonConvert.SerializeObject(list);
+
+                    }
+                }
+                catch
+                {
+
+                    return @"{""Status"":""ERRO""}";
+                }
+            }
+            else
+            {
+                return @"{""Status"":""Campos Vazios""}";
+            }
+        }
+    }
+}
---
+++ ApiFit/Models/Agenda.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PccAPI.Models
+{
+    public class Agenda
+    {
+        public string Nutricionista { get; set; }
+        public string Paciente { get; set; }
+        public int Id { get; set; }
+        public int Id_nutricionista { get; set; }
+        public int Id_paciente { get; set; }
+        public DateTime Dt_agenda { get; set; }   
+        public string Horario { get; set; }
+        public string Descricao { get; set; }
+
+    public Agenda(string r1, string r2, int r3, int r4, int r5, DateTime r6, string r7, string r8)
+        {
+            this.Nutricionista = r1;
+            this.Paciente = r2;
+            this.Id = r3;
+            this.Id_nutricionista = r4;
+            this.Id_paciente = r5;
+            this.Dt_agenda = r6;
+            this.Horario = r7;
+            this.Descricao = r8;
+        }
+    }
+}
